f98abf509e43bad8bcdbb0d81e808607f822b5c8
admin <aesenbekova8>
2019-03-16 18:07:27 +1100

    commit

--- src/main/java/ProjectCoWorking/models/Course.java
+++ src/main/java/ProjectCoWorking/models/Course.java
@@ -1,4 +1,58 @@
 package ProjectCoWorking.models;
 
 public class Course {
+    int id;
+    String name;
+    int rating;
+    String address;
+    double price;
+    String time;
+
+    public Course() {
+    }
+
+    public Course(int id, String name, int rating, String address, double price, String time) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.address = address;
+        this.price = price;
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rating=" + rating +
+                ", address='" + address + '\'' +
+                ", price=" + price +
+                ", time='" + time + '\'' +
+                '}';
+    }
 }
