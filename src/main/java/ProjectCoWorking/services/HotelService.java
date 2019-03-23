4cd069e3c7f351df97458adc22be44a86c2c2194
Shabdan <shabdan.abjaparov.official@gmail.com>
2019-03-23 14:02:03 +0600

    new services

---
+++ src/main/java/ProjectCoWorking/services/HotelService.java
@@ -0,0 +1,48 @@
+package ProjectCoWorking.services;
+
+import ProjectCoWorking.dao.HotelDao;
+import ProjectCoWorking.models.Hotel;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/hotels")
+public class HotelService {
+    @GET
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public List<Hotel> getAllHotels(){
+//        HotelDao.getAllHotels();
+        return null;
+    }
+
+    @GET
+    @Path("/{hotelId}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Hotel getHotel(@PathParam("hotelId") Integer hotelId){
+//        return null;
+        return HotelDao.getHotel(hotelId);
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Hotel createHotel(Hotel hotel){
+//        return HotelDao.addHotel(hotel);
+        return null;
+    }
+
+    @PUT
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Hotel updateHotel(Hotel hotel){
+//        return HotelDao.updateHotel(hotel);
+        return null;
+    }
+
+    @DELETE
+    @Path("/{hotelId}")
+    public void deleteHotel(@PathParam("hotelId") Integer hotelId){
+//        HotelDao.deleteHotel(hotelId);
+    }
+
+}
